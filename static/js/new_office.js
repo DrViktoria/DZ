cabcfaff7f35c4c99054e46c962b988103799adc
Виктория Дробышева <viktoria.dr15@gmail.com>
2017-01-16 10:08:49 +0300

    Add files via upload

--- static/js/new_office.js
+++ static/js/new_office.js
@@ -1,28 +1,41 @@
- function showError(container, errorMessage) {
-      container.className = 'error';
-      var msgElem = document.createElement('span');
-      msgElem.className = "error-message";
-      msgElem.innerHTML = errorMessage;
-      container.appendChild(msgElem);
-    }
+function validateNamed() {
+    var x = document.forms["NewOffice"]["named"].value;
 
-function resetError(container) {
-      container.className = '';
-      if (container.lastChild.className == "error-message") {
-        container.removeChild(container.lastChild);
-      }
-    }
-
-function validate(form) {
-    var elems = form.elements;
-
-    resetError(elems.named.parentNode);
-    if (!elems.named.value){
-        showError(elems.named.parentNode, 'укажите название')
-    }
-
-    resetError(elems.location.parentNode);
-    if (!elems.location.value){
-
+    if (x.length == 0) {
+        document.getElementById('id_named').innerHTML = "*укажите название";
+        return false;
+    } else {
+        document.getElementById('id_named').innerHTML = '';
+        return true;
     }
 }
+function validateLocation() {
+    var y = document.forms["NewOffice"]["location"].value;
+     if(y.length==0){
+        document.getElementById('id_location').innerHTML = "*укажите адрес";
+        return false;
+    } else {
+         document.getElementById('id_location').innerHTML='';
+         return true;
+     }
+
+}
+
+function  validatePhoto() {
+     var z = document.forms["NewOffice"]["picture"].value;
+
+     if(z.length==0){
+        document.getElementById('id_picture').innerHTML = "*загрузите логотип";
+        return false;
+    }else{
+         document.getElementById('id_picture').innerHTML='';
+         return true
+     }
+}
+
+function validate() {
+    if(validateNamed() && validateLocation() && validatePhoto()) return true;
+    else return false;
+
+}
+
